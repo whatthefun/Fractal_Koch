81e3e0f58da6f53b1d5849ce3e84aa388b94c4b7
whatthefun <tn9550803@gmail.com>
2017-11-12 12:16:30 +0800

    大致完成

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,7 +11,7 @@ import javafx.scene.shape.StrokeLineCap;
 public class Controller {
 
     private static final double MAX_LENGTH = 600;
-    private static final int  exterior_angles_sum = 360;
+    private static final int exterior_angles_sum = 360;
     private int polygon, level;
     private GraphicsContext gc;
 
@@ -23,59 +23,74 @@ public class Controller {
     public Label txtHint;
 
 
-    public void btnSubmitOnAction(){
+    public void btnSubmitOnAction() {
         System.out.println("btnSubmitOnAction");
 
-        verification();
-
-        paint();
+        if (verification()){
+            paint();
+        }
     }
 
-    private void verification(){
+    private boolean verification() {
         try {
             polygon = Integer.parseInt(edtPolygon.getText());
+            if (polygon < 3){
+                throw new NumberFormatException();
+            }
+
+            level = Integer.parseInt(edtLevel.getText());
+            if (level < -1){
+                throw new NumberFormatException();
+            }
+
             txtHint.setText("");
-        }catch (NumberFormatException e){
+
+            return true;
+        } catch (NumberFormatException e) {
             System.out.println("NumberFormatException");
             edtPolygon.setText("3 ~ 6");
-            txtHint.setText("Number Invalid!");
-        }
-
-        try {
-            level = Integer.parseInt(edtLevel.getText());
-            txtHint.setText("");
-        }catch (NumberFormatException e){
-            System.out.println("NumberFormatException");
             edtLevel.setText("0 ~ 5");
             txtHint.setText("Number Invalid!");
+
+            return false;
         }
     }
 
-    private void paint(){
+    //設定畫筆，並呼叫koch跑迴圈
+    private void paint() {
         gc = canvas.getGraphicsContext2D();
         gc.setLineCap(StrokeLineCap.BUTT);
-        gc.setLineWidth(3);
+        gc.setLineWidth(1);
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        koch(MAX_LENGTH -50, 450,
-                (MAX_LENGTH -100),180, level);
+        koch(MAX_LENGTH - 50, 450,
+                MAX_LENGTH - 100, -90, level, 1);
     }
 
-    private void koch(double startX, double startY, double length,int angle, int leftLevel){
-        if (leftLevel == -1){
-            gc.strokeLine(startX, startY, startX + length * Math.sin(Math.toRadians(angle)), startY + length * Math.cos(angle));
-        }else {
-            koch(startX, startY, length/3, angle, leftLevel-1);
-            double x = startX - length*2 /3;
-            double y = startY;
+    private void koch(double startX, double startY, double length, int angle, int leftLevel, int clockwise) {
+        if (leftLevel == -1) { //termination condition
+            gc.strokeLine(startX, startY, startX + length * Math.sin(Math.toRadians(angle)), startY + length * Math.cos(Math.toRadians(angle)));
+        } else {
+            koch(startX, startY, length / 3, angle, leftLevel - 1, clockwise);
+            double x = startX + length / 3 * Math.sin(Math.toRadians(angle)); //第二點
+            double y = startY + length / 3 * Math.cos(Math.toRadians(angle));
             int degree = angle;
-//            for (int i = 0; i < polygon - 2; i++){
-//                koch(x, y, length/3, degree + exterior_angles_sum/polygon, leftLevel-1);
-//                x += length/3 * Math.sin(degree);
-//                y += length/3 * Math.cos(degree);
-//                degree += exterior_angles_sum/polygon;
-//            }
-            koch(startX - length*2 /3, startY, length/3, angle, leftLevel-1);
+            if (clockwise < 0) {
+                for (int i = 0; i < polygon - 1; i++) {
+                    x += length / 3 * Math.sin(Math.toRadians(degree));
+                    y += length / 3 * Math.cos(Math.toRadians(degree));
+                    degree += exterior_angles_sum / polygon;
+                    koch(x, y, length / 3, degree, leftLevel - 1, clockwise * -1);
+                }
+            } else {
+                for (int i = 0; i < polygon - 1; i++) {
+                    x += length / 3 * Math.sin(Math.toRadians(degree));
+                    y += length / 3 * Math.cos(Math.toRadians(degree));
+                    degree -= exterior_angles_sum / polygon;
+                    koch(x, y, length / 3, degree, leftLevel - 1, clockwise * -1);
+                }
+            }
+            koch(startX + length * 2 / 3 * Math.sin(Math.toRadians(angle)), startY + length * 2 / 3 * Math.cos(Math.toRadians(angle)), length / 3, angle, leftLevel - 1, clockwise);
         }
     }
 }
